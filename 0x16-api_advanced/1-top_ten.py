e0754d227f2351f9b34d7ab326aebfccb8c5aa6b
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-05-07 21:15:02 +0300

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
-
-import urllib.request
-import json
+"""function that queries the Reddit API"""
+import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    request = urllib.request.Request(url, headers=headers)
-    with urllib.request.urlopen(request) as respose:
-        html = respose.read()
-        data = json.loads(html)
-        if respose.status == 200:
-            for post in data.get('data').get('children'):
-                print(post.get('data').get('title'))
-        else:
-            print(None)
+    """function that queries the Reddit API"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    # Provide a descriptive User-Agent header
+    headers = {"User-Agent": "My Reddit API Client"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+
+        for post in posts:
+            title = post["data"]["title"]
+            print(title)
+    else:
+        print("None")
